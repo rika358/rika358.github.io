dad64aecb5a620baa623ea15da8f976b8330d713
rika358 <168232265+rika358@users.noreply.github.com>
2024-05-08 05:44:42 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Java");
+  }
+}
+
+class Main {
+  public static void main(String[] args){
+  System.out.printiIn("Hello World");
+}
